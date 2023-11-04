7fb7df9757f8af0001405c00c179de70308a3260
Kobesnp <nshe0132@uni.sydney.edu.au>
2023-11-04 15:58:02 +1100

    Edison's first modify

--- final project/sketch.js
+++ final project/sketch.js
@@ -1,55 +1,135 @@
+let circles = [];
+
+function setup() {
+  createCanvas(457, 650);
+
+  rectMode(CORNERS);
+  strokeWeight(3);
+  circles.push(new HalfCircle(166, 482, 40, 0, PI, false));
+  circles.push(new HalfCircle(200, 482, 24, 0, PI, true));
+  circles.push(new HalfCircle(222, 468, 40, 1.1, PI + 2.1, true));
+  circles.push(new HalfCircle(252, 482, 30, 0, PI, false));
+  circles.push(new HalfCircle(288, 482, 40, 0, PI, true));
+  circles.push(new HalfCircle(237, 442, 23, 2.0, PI + 1.2, false));
+  circles.push(new HalfCircle(228, 415, 35, 1.3, 5, false));
+  circles.push(new HalfCircle(228, 370, 65, 1.4, 4.9, true));
+  circles.push(new HalfCircle(240, 316, 46, 1.8, 4.5, false));
+  circles.push(new HalfCircle(172, 267, 28, 0, PI, true));
+  circles.push(new HalfCircle(198, 270, 28, -0.2, PI + 0.2, false));
+  circles.push(new HalfCircle(225, 266, 28, 0.2, PI - 0.1, true));
+  circles.push(new HalfCircle(253, 275, 32, -0.3, PI + 0.4, false));
+  circles.push(new HalfCircle(280, 270, 25, 0, PI, false));
+  circles.push(new HalfCircle(307, 268, 34, 0.1, PI - 0.1, true));
+  circles.push(new HalfCircle(102, 48, 46, 1.7, PI + 1.3, false));
+  circles.push(new HalfCircle(94, 91, 46, 1.3, PI + 1.7, true));
+  circles.push(new HalfCircle(102, 128, 32, 1.7, PI + 1.4, false));
+  circles.push(new HalfCircle(128, 140, 30, 0.1, PI - 0.1, true));
+  circles.push(new HalfCircle(138, 164, 25, 1.5, PI + 1.8, false));
+  circles.push(new HalfCircle(132, 194, 40, 1.2, PI + 1.9, true));
+  circles.push(new HalfCircle(142, 240, 58, 1.7, PI + 1.5, false));
+  circles.push(new HalfCircle(207, 182, 18, 1.8, PI + 1.3, false));
+  circles.push(new HalfCircle(194, 200, 20, 0, PI, false));
+  circles.push(new HalfCircle(216, 200, 28, 0, PI, true));
+  circles.push(new HalfCircle(275, 183, 20, 1.5, PI + 1.6, false));
+  circles.push(new HalfCircle(265, 200, 25, 0, PI, false));
+  circles.push(new HalfCircle(240, 220, 40, 1.7, PI + 1.5, false));
+  circles.push(new HalfCircle(238, 245, 25, 1.7, PI + 1.5, true));
+  circles.push(new HalfCircle(410, 128, 20, 1.7, PI + 1.7, false));
+  circles.push(new HalfCircle(394, 144, 25, 0.5, PI + 0.3, false));
+  circles.push(new HalfCircle(370, 132, 36, 0.6, PI + 0.1, true));
+  circles.push(new HalfCircle(345, 128, 20, 0.3, PI + 0.1, false));
+  circles.push(new HalfCircle(326, 135, 25, 1.3, PI + 1.9, false));
+  circles.push(new HalfCircle(329, 170, 50, 1.6, PI + 1.6, true));
+  circles.push(new HalfCircle(322, 206, 35, 1.3, PI + 1.9, false));
+  circles.push(new HalfCircle(317, 236, 36, 1.1, PI + 2.1, false));
+
+}
 
 function draw() {
-  
-  translate(width / 2, height / 2);
-  background(207, 40, 50);
-  outpuApple(5, 6, 300, -200);
-  outpuApple(4, 3, 100, -500);
-  outpuApple(4, 5, 0, -400);
-  
-push();
-  //noStroke();
-  push();
-  stroke(40,80,90)
-  strokeWeight(5);
-  //竖
-  line(0,300,0,-100);//2
-  line(0,-100,0,-200);//4
-  line(180,-100,180,-360);//6
-  line(-180,-100,-180,-300);//8
-  line(-250,-300,-270,-450);//10
-  line(250,-350,250,-400);//12
-  line(-40,-200,-40,-235);//14
-  line(40,-200,40,-235);//16
+  background(255);
 
-  //横
+  // black stroke
+  noFill();
+  stroke(0);
+  for (let i = 0; i < circles.length; i++) {
+    circle(circles[i].x, circles[i].y, circles[i].diam + 2);
+  }
 
-  line(-180,-100,180,-100);//1
-  line(-40,-200,40,-200);//3
-  line(-40,-200,-60,-200);//5
-  line(-250,-300,-180,-300);//7
-  line(250,-350,180,-360);//9
-  pop();
+  // bottom
+  fill("#d7b764");
+  rect(104, 482, 333, 539);
+  stroke("#d7b764");
+  fill("#ea3e3e");
+  rect(148, 482, 186, 539);
+  fill("#519365");
+  rect(186, 482, 230, 539);
+  rect(266, 482, 308, 539);
 
+  noStroke();
+  fill("#519365");
+  arc(128, 539, 40, 40, PI, 2 * PI);
+  fill("#d7b764");
+  arc(168, 539, 40, 30, PI, 2 * PI);
+  fill("#ea3e3e");
+  stroke("#d7b764");
+  arc(208, 539, 40, 50, PI, 2 * PI);
+  noStroke();
+  arc(248, 539, 38, 40, PI, 2 * PI);
+  fill("#d7b764");
+  arc(286, 539, 38, 20, PI, 2 * PI);
+  fill("#519365");
+  arc(320, 539, 25, 25, PI, 2 * PI);
 
-  fill(40,80,90);
-  beginShape();
-  vertex(-200,400);
-  vertex(200,400);
-  vertex(200,300);
-  vertex(-200,300);
-  vertex(-200,400);
-  endShape();//yellow rect
-  
-  pop();
-  
-    outpuApple(16,1,40,-230.5);//line 16
-  for(n=0;n<7;n++){
-    outpuApple(1,n,-180+60*n,(scale[1][n]-50)-100);
-  }//line 1
-    outpuApple(3,1,-25,-200);
-    outpuApple(3,2,30,-200);//line 3
+  stroke(0);
+  noFill();
+  rect(104, 482, 333, 539);
 
+  noStroke();
+  for (let i = 0; i < circles.length; i++) {
+    circles[i].show();
+  }
 
+  // line
+  stroke("#d7b764");
+  line(145, 482, 306, 482);
+  line(230, 480, 238, 198);
+  line(136, 268, 323, 268);
+  line(143, 142, 136, 268);
+  line(143, 142, 100, 144);
+  line(96, 26, 100, 144);
+  line(330, 120, 323, 268);
+  line(330, 120, 406, 148);
+  line(410, 120, 406, 148);
+  line(183, 198, 277, 198);
+  line(274, 173, 277, 198);
+  line(204, 173, 203, 198);
+}
+
+// half filling circle
+class HalfCircle {
+  constructor(_x, _y, _diam, _sa, _ea, _fm) {
+    this.x = _x;
+    this.y = _y;
+    this.diam = _diam;
+    this.sa = _sa;
+    this.ea = _ea;
+    this.fm = _fm;
+  }
+
+  // show this circle
+  show() {
+    // stroke("#d7b764");
+    if (this.fm) {
+      fill("#ea3e3e");
+      arc(this.x, this.y, this.diam, this.diam, this.sa, this.ea, CHORD);
+      fill("#519365");
+      arc(this.x, this.y, this.diam, this.diam, this.ea, this.sa, CHORD);
+    } else {
+      fill("#519365");;
+      arc(this.x, this.y, this.diam, this.diam, this.sa, this.ea, CHORD);
+      fill("#ea3e3e");
+      arc(this.x, this.y, this.diam, this.diam, this.ea, this.sa, CHORD);
+    }
+  }
 }
 
